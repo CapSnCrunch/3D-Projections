09846bee8713e9b21e31f8b39ada138edb238d2a
Samuel Perales <samuelperales4@gmail.com>
2020-11-25 20:43:45 -0600

    Poly class added

--- main.py
+++ main.py
@@ -6,11 +6,36 @@ height = 750
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption('3D Projections')
 
-scale = 150
+scale = 100
 origin = (width / 2, height / 2)
 reference = [np.array([1, -1, 0]), np.array([1, 1, -2])]
 vectors = [np.array([1, 0, 0]), np.array([0, 1, 0]), np.array([0, 0, 1])]
 colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+
+class Poly():
+    def __init__(self, color, points, edges):
+        self.color = color
+        self.vertices = []
+        self.edges = edges
+
+        self.get_vertices(points)
+    
+    def get_vertices(self, points):
+        for p in points:
+            self.vertices.append(np.array(p))
+    
+    def draw(self, win):
+        #for i in range(len(self.vertices)):
+        #    for j in range(i, len(self.vertices)):
+        
+        for e in self.edges:
+            x1, y1 = proj(self.vertices[e[0]] * scale, reference[0], reference[1])
+            x2, y2 = proj(self.vertices[e[1]] * scale, reference[0], reference[1])
+            pygame.draw.line(win, self.color, (origin[0] + int(x1), origin[1] + int(y1)), (origin[0] + int(x2), origin[1] + int(y2)), 1)
+    
+    def rotate(self, r):
+        for i in range(len(self.vertices)):
+            self.vertices[i] = rotate(self.vertices[i], r)
 
 def rotate_x(u, theta):
     R = np.array([[1, 0, 0], [0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(theta)]])
@@ -38,11 +63,13 @@ def proj(u, vx, vy):
     # Change of Basis to Plane Vectors
     return (np.dot(w, vx) / np.sqrt(np.dot(vx, vx)), np.dot(w, vy) / np.sqrt(np.dot(vy, vy)))
 
-def redraw_window(win):
+def redraw_window(win, polyhedra):
     win.fill((250, 250, 250))
 
-    reference_rotate = [2*np.pi/150, 0, 2*np.pi/66]
-    polyhedra_rotate = [2*np.pi/150, 0, 0]
+    reference_rotate = [2*np.pi/200, 0, 2*np.pi/182]
+    # reference_rotate = [2*np.pi/150, 0, 0]
+    # polyhedra_rotate = [2*np.pi/150, 0, 0]
+    polyhedra_rotate = [0, 0, 0]
 
     for i in range(len(vectors)):
         # Rotate Reference
@@ -50,12 +77,20 @@ def redraw_window(win):
 
         x, y = proj(vectors[i] * scale, reference[0], reference[1])
         pygame.draw.line(win, colors[i], origin, (origin[0] + int(x), origin[1] + int(y)), 2)
-        
+    
+    for p in polyhedra:
+        p.rotate(reference_rotate)
+        p.rotate(polyhedra_rotate)
+        p.draw(win)
+
     pygame.display.update()
 
 def main():
     run = True
     clock = pygame.time.Clock()
+
+    Cube = Poly((0,0,0), [[-1,-1,-1], [-1,1,-1], [1,1,-1], [1,-1,-1], [-1,-1,1], [-1,1,1], [1,1,1], [1,-1,1]], 
+                          [[0, 1], [1, 2], [2, 3], [3, 0], [4, 5], [5, 6], [6, 7], [7, 4], [0, 4], [1, 5], [2, 6], [3, 7]])
 
     while run:
         clock.tick(30)
@@ -64,7 +99,7 @@ def main():
                 run = False
                 pygame.quit()
 
-        redraw_window(win)
+        redraw_window(win, [Cube])
 
 if __name__ == '__main__':
     main()
